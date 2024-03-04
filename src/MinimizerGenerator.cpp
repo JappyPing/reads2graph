b52f5a2644bc813409dc3e1400a28d4f6cdafd32
Jappy0 <ping.pengyao@gmail.com>
2024-03-04 23:35:52 +1100

    v0.6.0

--- src/MinimizerGenerator.cpp
+++ src/MinimizerGenerator.cpp
@@ -73,11 +73,11 @@ std::tuple<unsigned, unsigned, unsigned, double> MinimizerGenerator::possibleBet
     unsigned betterW;
     double p;
     if (args.read_length >= 8 && args.read_length < 16){
-        betterK = 3;
+        betterK = 4;
         betterN = 1;
         betterW = args.read_length;
     } else if (args.read_length >= 16 && args.read_length < 50){
-        betterK = 3;
+        betterK = 4;
         betterN = 2;
         betterW = round(args.read_length/betterN);
     } else if (args.read_length >= 50 && args.read_length <= 300) {
